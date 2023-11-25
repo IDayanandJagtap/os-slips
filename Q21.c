015b3d02a37df1885e6932fcb926049d7379aa53
Dayanand Jagtap <dayanandjagtap07@gmail.com>
2023-11-25 18:43:46 +0530

    Slip 21

--- Q21.c
+++ Q21.c
@@ -1,10 +1,20 @@
+/*
+
+Write a C program that accepts an integer array. Main function forks child
+ process. Parent process sorts an integer array and passes the sorted array to
+ child process through the command line arguments of execve() system call.
+ The child process uses execve() system call to load new program that uses this
+ sorted array for performing the binary search to search the particular item in
+ the array
+*/
+
 #include <stdio.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 
-int main()
+void main()
 {
     int arr[] = {1, 2, 3, 4, 5, 6, 7};
     int size = 7;
@@ -12,7 +22,7 @@ int main()
 
     if (fork() == 0)
     {
-        char *argv[size + 3];
+        char *argv[size + 4];
 
         char sizeInChar[10];
         char keyInChar[10];
@@ -20,46 +30,27 @@ int main()
         sprintf(sizeInChar, "%d", size);
         sprintf(keyInChar, "%d", key);
 
-        argv[0] = strdup("binarySearch"); // Corrected executable name
+        argv[0] = strdup("binarySearch");
         argv[1] = strdup(sizeInChar);
         argv[2] = strdup(keyInChar);
 
-        // Check for NULL after each strdup
-        if (argv[0] == NULL || argv[1] == NULL || argv[2] == NULL)
-        {
-            perror("strdup failed");
-            exit(EXIT_FAILURE);
-        }
-
         char str[10]; // to copy the sorted array
-        for (int i = 0; i < size; i++)
+        int i = 0;
+        for (i = 0; i < size; i++)
         {
             sprintf(str, "%d", arr[i]);
             argv[i + 3] = strdup(str);
-
-            // Check for NULL after each strdup
-            if (argv[i + 3] == NULL)
-            {
-                perror("strdup failed");
-                exit(EXIT_FAILURE);
-            }
         }
 
-        // Explicitly null-terminate the args array
         argv[size + 3] = NULL;
-
         execve("./binarySearch", argv, NULL);
-
-        // If execve fails
+        // printf("Some thingi went wrong\n");
         perror("Execve failed");
-        exit(EXIT_FAILURE);
     }
     else
     {
         int status;
         wait(&status);
-        // printf("Exited with status: %d\n", status);
+        // printf("Exited with status : %d\n", status);
     }
-
-    return 0;
 }
--- binarySearch.c
+++ binarySearch.c
@@ -1,33 +1,74 @@
+// binarySearch.c
+
 #include <stdio.h>
+#include <stdlib.h>
 
-void main(int argc, char *argv[])
+int binarySearch(int arr[], int size, int key)
 {
-    int *sortedArray = atoi(argv[1]);
-    int size = atoi(argv[1][size - 2]);
-    int key = atoi(argv[1][size - 1]);
-
-        int low = 0;
-    int high = size - 1;
-    int mid;
+    int low = 0, high = size - 1;
 
     while (low <= high)
     {
-        mid = (low + high) / 2;
+        int mid = low + (high - low) / 2;
 
-        if (sortedArray[mid] == key)
-        {
-            printf("Item %d found at index %d\n", key, mid);
-            return;
-        }
-        else if (sortedArray[mid] < key)
-        {
+        if (arr[mid] == key)
+            return mid;
+
+        if (arr[mid] < key)
             low = mid + 1;
-        }
         else
-        {
             high = mid - 1;
-        }
     }
 
-    printf("Item %d not found in the array\n", key);
+    return -1; // Key not found
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
+        fprintf(stderr, "Usage: %s <size> <key> <sorted_array_elements...>\n", argv[0]);
+        return 1;
+    }
+
+    int size = atoi(argv[1]);
+    int key = atoi(argv[2]);
+
+    if (argc != size + 3)
+    {
+        fprintf(stderr, "Incorrect number of array elements provided.\n");
+        return 1;
+    }
+
+    int *arr = malloc(size * sizeof(int));
+
+    if (!arr)
+    {
+        perror("Memory allocation error");
+        return 1;
+    }
+
+    // Convert command line arguments to integers and fill the array
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = atoi(argv[i + 3]);
+    }
+
+    // Perform binary search
+    int result = binarySearch(arr, size, key);
+
+    // Display the result
+    if (result != -1)
+    {
+        printf("Key %d found at index %d.\n", key, result);
+    }
+    else
+    {
+        printf("Key %d not found in the array.\n", key);
+    }
+
+    // Free allocated memory
+    free(arr);
+
+    return 0;
 }
